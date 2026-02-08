f2d629a1fbc1558ccc9752650ec55507fd134760
Zziniswell <handmamy22@gmail.com>
2026-02-08 20:03:54 +0900

    Initial commit with current state

---
+++ Gallery Extension.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Gallery Extension for FMKOREA
+// @version      3.23
+// @description  (모바일) 사이트 좌측 상단에서 메뉴를 열어주세요. PC는 일부 옵션만 자동 적용됩니다.
+// @author       cent8649
+// @match        https://*.fmkorea.com/*
+// @run-at       document-start
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        unsafeWindow
+// @icon         https://www.google.com/s2/favicons?domain=fmkorea.com
+// @homepage     https://gall.dcinside.com/mgallery/board/view/?id=adguard&no=2348
+// @downloadURL  https://github.com/Zziniswell/Adguard-gallery-filter/raw/refs/heads/main/Gallery%20Extension.user.js
+// @updateURL    https://github.com/Zziniswell/Adguard-gallery-filter/raw/refs/heads/main/Gallery%20Extension.user.js
+// ==/UserScript==
+
+(function() {})()
